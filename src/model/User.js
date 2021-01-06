ed786d71683d8662594f93a4760c72d671d261f6
v_huuuichen <v_huuuichen@tencent.com>
2021-01-06 20:08:44 +0800

    'addcode'

--- src/model/User.js
+++ src/model/User.js
@@ -3,10 +3,33 @@ import mongoose from '@/config/DBHelpler'
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  'username': { type: String },
-  'name': { type: String },
-  'password': { type: String },
+  'username': {
+    type: String
+  },
+  'name': {
+    type: String
+  },
+  'password': {
+    type: String
+  },
 })
+
+UserSchema.statics = {
+  findByID: function (id) {
+    return this.findOne({
+      _id: id
+    }, {
+      password: 0,
+      username: 0,
+      mobile: 0
+    })
+  },
+  updateOneByID: function (id, obj) {
+    return this.updateOne({
+      _id: id
+    }, obj)
+  }
+}
 
 const UserModel = mongoose.model('users', UserSchema)
 
